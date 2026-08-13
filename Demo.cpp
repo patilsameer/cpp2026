69761eb468b79f477913a3a6098260c348d0d8f3
sameer patil <sbpatil29@gmail.com>
2026-08-13 13:18:02 +0530

    Demo.cpp

---
+++ Demo.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Demo{
+	
+	public:
+		
+			Demo(){
+					cout<<"I am a constructor"<<endl;
+			}
+
+			int i;
+
+		
+
+};
+
+
+int main(){
+		
+		Demo d;
+
+	
+}
